18905631de67a8ed53c202c961cb8c9104867be9
Max <elmimax@yahoo.it>
2026-08-12 10:15:36 +0200

    cards update

--- js/cards.js
+++ js/cards.js
@@ -2,7 +2,7 @@ const setMap = {
     SVI: "sv01",
     PAL: "sv02",
     OBF: "sv03",
-    MEW: "sv3.05",
+    MEW: "sv03.05",
     PAR: "sv04",
     PAF: "sv04.5",
     TEF: "sv05",
@@ -17,7 +17,7 @@ const setMap = {
     BLK: "sv10.5b",
     MEG: "me01",
     PFL: "me02",
-    ASC: "me2.5",
+    ASC: "me02.5",
     POR: "me03",
     CRI: "me04",
     PBL: "me05"
@@ -25,34 +25,33 @@ const setMap = {
 
 // Ora puoi scrivere le tue staples usando I CODICI DEL SET UFFICIALI (es. "ASC-142")
 const staples = [
-    "SFA-038",
-    "ASC-196",
-    "POR-062", 
-    "TWM-095", 
-    "MEG-119",
-    "MEG-114",
-    "MEG-131",
-    "POR-081",
-    "MEG-125",
-    "DRI-176"
+    "SFA-038", // 1 Fezandipiti ex
+    "POR-062", // 2 Meowth ex
+    "TWM-095", // 3 Munkidori
+    "MEG-119", // 4 Lillie's Determination
+    "MEG-114", // 5 Boss's Orders
+    "MEG-131", // 6 Ultra Ball
+    "POR-081", // 7 Poké Pad
+    "MEG-125", // 8 Rare Candy
+    "DRI-176" // 9 Team Rocket's Petrel
     
 ];
 
 const useful = [
-    "JTG-056",
-    "ASC-039",
-    "JTG-120",
-    "TEF-129",
-    "TEF-144",
-    "SVI-186",
-    "ASC-181",
-    "DRI-010",
-    "PFL-087",
-    "WHT-084",
-    "ASC-196",
-    "CRI-082",
-    "SCR-133",
-    "TWM-165"
+    "JTG-056", //1 Lillie's Clefairy ex
+    "ASC-039", //2
+    "JTG-120", //3 Dunsparce
+    "TEF-129", //4 Dudunsparce
+    "TEF-144", //5 Buddy-Buddy Poffin
+    "SVI-186", //6 Pokégear 3.0
+    "BLK-079", //7 Air Balloon ^
+    "DRI-010", //8 Shaymin
+    "PFL-087", //9 Dawn
+    "WHT-084", //10 Hilda
+    "ASC-196", //11
+    "CRI-082", //12 Special Red Card
+    "SCR-133", //13 Crispin
+    "TWM-165" //14 Unfair Stamp
 
 ]
 
